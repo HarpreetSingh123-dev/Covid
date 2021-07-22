38d71aea2d0c2813fa8327671f9c8722560f1b73
Harpreet Singh <hsk54088@gmail.com>
2021-07-22 13:39:28 -0600

    News Component Fully Set

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -4,6 +4,8 @@ import './News.css'
 import Skeleton from '@yisheng90/react-loading';
 import axios from 'axios'
 import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+
 
 function News(props) {
 
@@ -23,14 +25,21 @@ function News(props) {
  
  const [pageNumber, setPageNumber] = useState(0)
 
- const [loader , setLoader] = useState(true)
+ const [vaccineButtonColor , setVaccineButtonColor] = useState('grey')
+
+ const [covidButtonColor, setCovidButtonColor] = useState('')
+
+ const [healthButtonColor , setHealthButtonColor] = useState('')
+
+ const [loader, setLoader] = useState()
+
 
  
 
   
  useEffect(()=>{
 
-    
+   
      
     var options1 = {
         method: 'GET',
@@ -47,7 +56,8 @@ function News(props) {
         setVaccineNews(response.data.news)
         
 
-    }).catch(function (error) {
+    })
+     .catch(function (error) {
         console.error(error);
     });
 
@@ -87,8 +97,8 @@ function News(props) {
         console.error(error);
     }); 
 
-setLoader(false)
-  
+
+    
 
     },[pageNumber])
 
@@ -98,36 +108,46 @@ setLoader(false)
 
 if(vaccineComponent){
 
-              var previousButton
+       
+             var previousButton
 
              if(pageNumber === 0){
                         previousButton = null
                      }
 
              else{
-                    previousButton =(<button onClick={ ()=>setPageNumber(pageNumber-1)}>Previous</button>)
+                    previousButton =(<button type="button" class="btn btn-info" onClick={ ()=>setPageNumber(  prevState => prevState - 1)}>Previous</button>)
                  }
 
-               if(loader){
-                display =(<div><Skeleton rows={6} color="lightgray"></Skeleton></div>)
-               }
+  
+              display = ( <div> 
 
-               else{
-              display = ( <div>
+                           <div className="navigationButtonSet">
+                           {previousButton}
+                           <button type="button" class="btn btn-info" onClick={ ()=>setPageNumber( prevState => prevState + 1)}>Next</button>
+                           </div>
 
+                           <div className="setScroll">
                            {vaccineNews.map((news)=>{
 
-                                return <li>{news.title}</li>
+                                return <div class="card mb-3">
+                                       <img class="card-img-top" src={news.urlToImage} alt="Card image cap"></img>
+                                       <div class="card-body">
+                                       <h5 class="card-title">{news.title}</h5>
+                                       <p class="card-text">{news.content}</p>
+                                       <p class="card-text"><a href={news.link} target="_blank">Click Here for more</a></p>
+                                       <p class="card-text"><small class="text-muted" >{news.pubDate}</small></p>
+                                       </div>
+                                       </div>
                            })}
-
-                           {previousButton}
-                           <button onClick={ ()=>setPageNumber(pageNumber+1)}>Next</button>
-                     
+                          </div>
                       </div> 
-                     )}
-}
+                     )}       
+          
 
 if(healthComponent){
+
+       
 
              var previousButton
 
@@ -136,25 +156,39 @@ if(healthComponent){
                 }
 
             else{
-              previousButton =(<button onClick={ ()=>setPageNumber(pageNumber-1)}>Previous</button>)
+              previousButton =(<button type="button" class="btn btn-info" onClick={ ()=>setPageNumber(prevState => prevState - 1)}>Previous</button>)
              }
 
             display = ( <div>
 
+                       <div className="navigationButtonSet">
+                       {previousButton}
+                       <button type="button" class="btn btn-info" onClick={ ()=>setPageNumber(prevState => prevState + 1)}>Next</button>
+                       </div>
+
                           {healthNews.map((news)=>{
 
-                                 return <li>{news.title}</li>
+                                 return <div class="card mb-3">
+                                        <img class="card-img-top" src={news.urlToImage} alt="Card image cap"></img>
+                                        <div class="card-body">
+                                        <h5 class="card-title">{news.title}</h5>
+                                        <p class="card-text">{news.content}</p>
+                                        <p class="card-text"><a href={news.link} target="_blank">Click Here for more</a></p>
+                                        <p class="card-text"><small class="text-muted" >{news.pubDate}</small></p>
+                                        </div>
+                                       </div>
          
                          })}
 
-                       {previousButton}
-                       <button onClick={ ()=>setPageNumber(pageNumber+1)}>Next</button>
+                       
                       
                       </div>
                      )
 }
 
 if(covidComponent){
+           
+                
 
                 var previousButton
 
@@ -163,46 +197,89 @@ if(covidComponent){
                   } 
 
                 else{
-                    previousButton =(<button onClick={ ()=>setPageNumber(pageNumber-1)}>Previous</button>)
+                    previousButton =(<button type="button" class="btn btn-info "  style={{marginLeft:'0'}} onClick={ ()=>setPageNumber(prevState => prevState - 1)}>Previous</button>)
                    }
   
                display = ( <div>
+                          
+                          <div className="navigationButtonSet">
+                          {previousButton}
+                          <button type="button" class="btn btn-info" style={{marginRight:'0'}} onClick={ ()=>setPageNumber(prevState => prevState + 1)}>Next</button>
+                          </div>
 
                            {covidNews.map((news)=>{
 
-                                 return <li>{news.title}</li>
+                                 return <div class="card mb-3">
+                                        <img class="card-img-top" src={news.urlToImage} alt="Card image cap"></img>
+                                        <div class="card-body">
+                                        <h5 class="card-title">{news.title}</h5>
+                                        <p class="card-text">{news.content}</p>
+                                        <p class="card-text"><a href={news.link} target="_blank">Click Here for more</a></p>
+                                        <p class="card-text"><small class="text-muted" >{news.pubDate}</small></p>
+                                        </div>
+                                        </div>
         
                             })}
 
-                          {previousButton}
-                          <button onClick={ ()=>setPageNumber(pageNumber+1)}>Next</button>
+                          
                           </div>
                         )
 
 }
 
   const vaccineNewsSet = ()=>{
+  
+    
 
     setHealthComponent(false)
     setCovidComponent(false)
     setVaccineComponent(true)
+
+    setPageNumber(0)
+
+    /* side button color logic */
+
+    setVaccineButtonColor('grey')
+    setCovidButtonColor('')
+    setHealthButtonColor('')
+
+
+    
+
+    
+    
   }  
 
   const healthNewsSet = ()=>{
+  
+    
 
     setHealthComponent(true)
     setCovidComponent(false)
     setVaccineComponent(false)
-     
+
+    setPageNumber(0)
+
+    setVaccineButtonColor('')
+    setCovidButtonColor('')
+    setHealthButtonColor('grey')
+    
 }
 
 
   const covidNewsSet = ()=>{
-  
+   
+    
     setHealthComponent(false)
     setCovidComponent(true)
     setVaccineComponent(false)
-      
+
+    setPageNumber(0)
+
+    setVaccineButtonColor('')
+    setCovidButtonColor('grey')
+    setHealthButtonColor('')
+   
   }
 
  
@@ -220,48 +297,50 @@ if(covidComponent){
 
               <div className="row">
 
-                  <div className="col-lg-2 col-md-3 col-sm-12 test sideButtons">
+                  <div className="col-lg-2 col-md-3 col-sm-12 sideButtons test ">
+
 
                     <div className="row">
-                    <button type="button" class="btn btn-primary btn-lg btnSet" onClick={vaccineNewsSet}>VACCINE</button>
+                    <button type="button" class="btn btn-primary btn-lg btnSet" style={{backgroundColor: vaccineButtonColor}} onClick={vaccineNewsSet}>VACCINE</button>
                     </div>
 
                     <div className="row">
-                    <button type="button" class="btn btn-primary btn-lg btnSet" onClick={covidNewsSet}>COVID-19</button>
+                    <button type="button" class="btn btn-primary btn-lg btnSet " style={{backgroundColor: covidButtonColor}} onClick={covidNewsSet}>COVID-19</button>
                     </div>
 
                     <div className="row">
-                    <button type="button" class="btn btn-primary btn-lg btnSet" onClick={healthNewsSet}>HEALTH</button>
+                    <button type="button" class="btn btn-primary btn-lg btnSet " style={{backgroundColor: healthButtonColor}} onClick={healthNewsSet}>HEALTH</button>
                     </div>
-                  
-                  
+                
                  
 
 
                   </div>
 
                   <div className="col-lg-10 col-md-9 col-sm-12 test">
-                      <h1>Content here</h1>
-
-                      {display}
+                    
+                        <div className="display">
+                          {display}
+                        </div>
 
                   </div>   
 
-            {/*
-
-            {console.log("vaccine news below")}
-            {console.log(vaccineNews)}
-            {console.log(healthNews)}
-            {console.log(covidNews)}  */}
-{console.log(vaccineNews)}
               </div>
 
             
 
             </div> 
 
-        </div>
+              <div className="breakSet">
 
+
+              </div>
+
+              <div className="footerSet">
+                <Footer></Footer>
+              </div> 
+        
+        </div>
       </div>
 
     );
