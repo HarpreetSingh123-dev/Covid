89567689642805126cbe62a9df58394c49f5d00f
Harpreet Singh <hsk54088@gmail.com>
2021-10-18 12:55:05 -0600

    for testing news component on small screens

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -596,7 +596,7 @@ setFooterDataType(value){
 
                    <hr className="rule"></hr>
                     
-                   <MainPageNews></MainPageNews>
+                  {/* <MainPageNews></MainPageNews>*/}
 
                </div>
 
